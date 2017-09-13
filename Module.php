43e9dd52d0cb2b16ee586f8a890700aa13931d82
Emil Grabowiec <emil@aslaninteractive.com>
2017-09-13 11:24:51 +0200

    updated source files

---
+++ Module.php
@@ -0,0 +1,28 @@
+<?php
+namespace WwseHtmlReplace;
+
+class Module
+{
+    public function onBootstrap($e)
+    {
+	    $eventManager = $e->getApplication()->getEventManager ();
+
+	    $eventManager->attach(MvcEvent::EVENT_FINISH, function(MvcEvent $e) {
+		    $cfg = $e->getApplication()->getServiceManager()->get('config');
+
+	    	if(array_key_exists('wws_html_replace', $cfg)) {
+			    $controller = $e->getRouteMatch()->getParam('controller');
+			    $action     = $e->getRouteMatch()->getParam('action');
+			    $class      = $cfg['controllers']['invokables'][$controller];
+			    $crowlerCfg = $cfg['wwsse_html_replace'];
+
+			    if(array_key_exists($class, $crowlerCfg) && array_key_exists($action, $crowlerCfg[$class])) {
+				    $content = $e->getResponse()->getContent();
+				    $crowler = HtmlPageCrawler::create($content);
+
+				    $e->getResponse()->setContent($crowlerCfg[$class][$action]($crowler));
+			    }
+		    }
+	    });
+    }
+}
