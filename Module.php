7ca3b1eedd205d5767a5c71b551a1f2be2e66371
Emil Grabowiec <emil@aslaninteractive.com>
2017-09-13 11:34:20 +0200

    minor fixes

--- Module.php
+++ Module.php
@@ -1,6 +1,8 @@
 <?php
 namespace WwseHtmlReplace;
 
+use Zend\Mvc\MvcEvent;
+
 class Module
 {
     public function onBootstrap($e)
@@ -10,7 +12,7 @@ class Module
 	    $eventManager->attach(MvcEvent::EVENT_FINISH, function(MvcEvent $e) {
 		    $cfg = $e->getApplication()->getServiceManager()->get('config');
 
-	    	if(array_key_exists('wws_html_replace', $cfg)) {
+	    	if(array_key_exists('wwsse_html_replace', $cfg)) {
 			    $controller = $e->getRouteMatch()->getParam('controller');
 			    $action     = $e->getRouteMatch()->getParam('action');
 			    $class      = $cfg['controllers']['invokables'][$controller];
