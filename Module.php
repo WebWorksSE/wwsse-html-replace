986c9dcba1ac1b440c19260f142218cefd51953d
Emil Grabowiec <emil@aslaninteractive.com>
2017-09-20 16:35:34 +0200

    fixed bug with getting invokable controller class

--- Module.php
+++ Module.php
@@ -6,39 +6,42 @@ use Zend\Mvc\MvcEvent;
 
 class Module
 {
-    public function onBootstrap($e)
-    {
-	    $eventManager = $e->getApplication()->getEventManager ();
+	public function onBootstrap($e)
+	{
+		$eventManager = $e->getApplication()->getEventManager ();
 
-	    $eventManager->attach(MvcEvent::EVENT_FINISH, function(MvcEvent $e) {
-		    $cfg = $e->getApplication()->getServiceManager()->get('config');
+		$eventManager->attach(MvcEvent::EVENT_FINISH, function(MvcEvent $e) {
+			$cfg = $e->getApplication()->getServiceManager()->get('config');
 
-	    	if(array_key_exists('wwsse_html_replace', $cfg)) {
-			    $route = $e->getRouteMatch();
+			if(array_key_exists('wwsse_html_replace', $cfg)) {
+				$route = $e->getRouteMatch();
 
-			    if(!is_null($route)) {
-				    $controller = $route->getParam('controller');
-				    $action     = $route->getParam('action');
-				    $class      = $cfg['controllers']['invokables'][$controller];
-				    $crowlerCfg = $cfg['wwsse_html_replace'];
+				if(!is_null($route)) {
+					$controller = $route->getParam('controller');
+					$action     = $route->getParam('action');
 
-				    /* controller/action specific html modification */
-				    if(array_key_exists($class, $crowlerCfg) && array_key_exists($action, $crowlerCfg[$class])) {
-					    $content    = $e->getResponse()->getContent();
-					    $crowler    = HtmlPageCrawler::create($content);
+					if(array_key_exists($controller, $cfg['controllers']['invokables'])) {
+						$class      = $cfg['controllers']['invokables'][$controller];
+						$crowlerCfg = $cfg['wwsse_html_replace'];
 
-					    $e->getResponse()->setContent($crowlerCfg[$class][$action]($crowler));
-				    }
+						/* controller/action specific html modification */
+						if(array_key_exists($class, $crowlerCfg) && array_key_exists($action, $crowlerCfg[$class])) {
+							$content    = $e->getResponse()->getContent();
+							$crowler    = HtmlPageCrawler::create($content);
 
-				    /* global specific html modification */
-				    if(array_key_exists('global', $crowlerCfg)) {
-					    $content    = $e->getResponse()->getContent();
-					    $crowler    = HtmlPageCrawler::create($content);
+							$e->getResponse()->setContent($crowlerCfg[$class][$action]($crowler));
+						}
 
-					    $e->getResponse()->setContent($crowlerCfg['global']($crowler));
-				    }
-			    }
-		    }
-	    });
-    }
+						/* global specific html modification */
+						if(array_key_exists('global', $crowlerCfg)) {
+							$content    = $e->getResponse()->getContent();
+							$crowler    = HtmlPageCrawler::create($content);
+
+							$e->getResponse()->setContent($crowlerCfg['global']($crowler));
+						}
+					}
+				}
+			}
+		});
+	}
 }
